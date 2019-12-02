646c6f0513844bd24e5bfa01fa3a59d886868ff6
calexchable <51465943+calexchable@users.noreply.github.com>
2019-12-02 12:21:24 -0600

    Reworking SearchController, POST Method, calex.

--- src/main/java/com/example/GetSaucedApp/controllers/SearchController.java
+++ src/main/java/com/example/GetSaucedApp/controllers/SearchController.java
@@ -6,8 +6,12 @@ import com.example.GetSaucedApp.models.data.HotSauceDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "hot-sauces")
@@ -18,6 +22,7 @@ public class SearchController {
     private HotSauceDao hotSauceDao;
 
 //    @Autowired
+//    The Autowire did something to the program where I was not able to bootRun the thing (needed to do something else... don't remember //
     private HotSauceDaoImpl hotSauceDaoImpl;
 
     @RequestMapping(value = "search", method = RequestMethod.GET)
@@ -27,39 +32,22 @@ public class SearchController {
 
         return "hot-sauces/search";
     }
+
+    @RequestMapping(value = "search", method = RequestMethod.POST)
+    public String SearchHotSauces(@ModelAttribute @Valid SearchForm newSearchForm,
+                                  Errors errors,
+                                  Model model) {
+
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Try Searching the Sauces Again!");
+            return "hot-sauces/search";
+        }
+
+//        Missing something to do with the searchForm, I need to pass the information.... somehow "
+
+        return "redirect:results";
+    }
 }
-
-//    @RequestMapping(value = "search", method = RequestMethod.POST)
-//    public String searchHotSauces(@ModelAttribute @Valid SearchForm searchForm,
-//                                  Errors errors,
-//                                  Model model) {
-//
-//        ArrayList<HotSauce> hotSauces = new ArrayList<>();
-//
-//        model.addAttribute("title", "Search the Sauces!");
-//        model.addAttribute(new SearchForm());
-//        model.addAttribute("hotSauces", hotSauces);
-//
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Try Searching the Sauces Again!");
-//            return "hot-sauces/search";
-//
-//        }
-//        else {
-//
-//            if (searchForm.getSearchField().equals(SearchCategory.ALL)) {
-//                hotSauceDaoImpl.findByValue(searchForm.getSearchField(), searchForm.getKeyword());
-//            } else {
-//                hotSauceDaoImpl.findByColumnAndValue(searchForm.getSearchField(), searchForm.getKeyword());
-//            }
-
-
-//        }
-//
-//
-//        return "redirect:results";
-//    }}
-//
 //    @RequestMapping(value = "results")
 //    public String resultingHotSauces(Model model) {
 //
