9a5bddd63fee556bef01f73d19863713b049d2c4
calexchable <51465943+calexchable@users.noreply.github.com>
2019-12-02 12:08:12 -0600

    Reworking SearchController, GET Method finished, calex.

--- src/main/java/com/example/GetSaucedApp/controllers/SearchController.java
+++ src/main/java/com/example/GetSaucedApp/controllers/SearchController.java
@@ -1,20 +1,13 @@
 package com.example.GetSaucedApp.controllers;
 
-import com.example.GetSaucedApp.models.HotSauce;
-import com.example.GetSaucedApp.models.SearchCategory;
 import com.example.GetSaucedApp.models.SearchForm;
 import com.example.GetSaucedApp.models.data.HotSauceDao;
 import com.example.GetSaucedApp.models.data.HotSauceDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.validation.Valid;
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping(value = "hot-sauces")
@@ -23,6 +16,8 @@ public class SearchController {
 
     @Autowired
     private HotSauceDao hotSauceDao;
+
+//    @Autowired
     private HotSauceDaoImpl hotSauceDaoImpl;
 
     @RequestMapping(value = "search", method = RequestMethod.GET)
@@ -32,42 +27,60 @@ public class SearchController {
 
         return "hot-sauces/search";
     }
+}
 
-    @RequestMapping(value = "search", method = RequestMethod.POST)
-    public String searchHotSauces(@ModelAttribute @Valid SearchForm newSearchForm,
-                                  Errors errors,
-                                  Model model) {
-
-        model.addAttribute("title", "Search the Sauces!");
-        model.addAttribute(new SearchForm());
-
+//    @RequestMapping(value = "search", method = RequestMethod.POST)
+//    public String searchHotSauces(@ModelAttribute @Valid SearchForm searchForm,
+//                                  Errors errors,
+//                                  Model model) {
+//
+//        ArrayList<HotSauce> hotSauces = new ArrayList<>();
+//
+//        model.addAttribute("title", "Search the Sauces!");
+//        model.addAttribute(new SearchForm());
+//        model.addAttribute("hotSauces", hotSauces);
+//
 //        if (errors.hasErrors()) {
 //            model.addAttribute("title", "Try Searching the Sauces Again!");
 //            return "hot-sauces/search";
 //
 //        }
-
-        return "redirect:results";
-    }
-
-    @RequestMapping(value = "results", method = RequestMethod.GET)
-    public String resultingHotSauces(Model model,
-                                  @ModelAttribute SearchForm searchFrom) {
-
-        ArrayList<HotSauce> hotSauces;
-        SearchForm searchForm = new SearchForm();
-
-        if (searchForm.getSearchField().equals(SearchCategory.ALL)) {
-            hotSauces = hotSauceDaoImpl.findByValue(searchForm.getSearchField(), searchForm.getKeyword());
-
-        } else {
-            hotSauces = hotSauceDaoImpl.findByColumnAndValue(searchForm.getSearchField(), searchForm.getKeyword());
-        }
-
-        model.addAttribute("hotSauces", hotSauces);
+//        else {
+//
+//            if (searchForm.getSearchField().equals(SearchCategory.ALL)) {
+//                hotSauceDaoImpl.findByValue(searchForm.getSearchField(), searchForm.getKeyword());
+//            } else {
+//                hotSauceDaoImpl.findByColumnAndValue(searchForm.getSearchField(), searchForm.getKeyword());
+//            }
 
 
-
-        return "hot-sauces/results";
-    }
-}
+//        }
+//
+//
+//        return "redirect:results";
+//    }}
+//
+//    @RequestMapping(value = "results")
+//    public String resultingHotSauces(Model model) {
+//
+//        ArrayList<HotSauce> hotSauces = new ArrayList<>();
+//
+//        model.addAttribute("title", "Check out your search results!");
+//        model.addAttribute("hotSauces", hotSauces);
+//
+//        SearchForm searchForm = new SearchForm();
+//
+//        if (searchForm.getSearchField().equals(SearchCategory.ALL)) {
+//            hotSauces = hotSauceDaoImpl.findByValue(searchForm.getSearchField(), searchForm.getKeyword());
+//
+//        } else {
+//            hotSauces = hotSauceDaoImpl.findByColumnAndValue(searchForm.getSearchField(), searchForm.getKeyword());
+//        }
+//
+//        model.addAttribute("hotSauces", hotSauces);
+//
+//
+//
+//        return "hot-sauces/results";
+//    }
+//}
