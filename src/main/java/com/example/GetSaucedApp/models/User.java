5b6613ae39dd1cb2597f1557d326004350123e39
calexchable <51465943+calexchable@users.noreply.github.com>
2019-12-03 20:45:47 -0600

    Security, added RoleRepository interface, calex.

--- src/main/java/com/example/GetSaucedApp/models/User.java
+++ src/main/java/com/example/GetSaucedApp/models/User.java
@@ -3,7 +3,6 @@ package com.example.GetSaucedApp.models;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.management.relation.Role;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
--- src/main/java/com/example/GetSaucedApp/models/data/UserRepository.java
+++ src/main/java/com/example/GetSaucedApp/models/data/UserRepository.java
@@ -4,7 +4,7 @@ import com.example.GetSaucedApp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
