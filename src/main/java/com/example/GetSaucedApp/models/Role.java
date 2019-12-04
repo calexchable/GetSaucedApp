e763eb217715409bf6879b6e4d4970db96c81521
calexchable <51465943+calexchable@users.noreply.github.com>
2019-12-04 12:51:49 -0600

    Security, added LoginController, calex.

--- src/main/java/com/example/GetSaucedApp/models/Role.java
+++ src/main/java/com/example/GetSaucedApp/models/Role.java
@@ -16,4 +16,5 @@ public class Role {
 
     @Column(name = "role")
     private String role;
+
 }
--- src/main/java/com/example/GetSaucedApp/models/User.java
+++ src/main/java/com/example/GetSaucedApp/models/User.java
@@ -30,7 +30,7 @@ public class User {
 
     @Column(name = "first_name")
     @NotEmpty(message = "Please provide your name")
-    private String name;
+    private String firstName;
 
     @Column(name = "last_name")
     @NotEmpty(message = "Please provide your last name")
